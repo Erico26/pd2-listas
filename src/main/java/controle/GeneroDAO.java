f0539c0701ccee69ab549c6fb141065037db04b9
Eric <eric.o26@aluno.ifsc.edu.br>
2023-08-08 09:04:12 -0300

    arrumando2

--- src/main/java/controle/GeneroDAO.java
+++ src/main/java/controle/GeneroDAO.java
@@ -17,16 +17,29 @@ public class GeneroDAO {
 		//abrir instancia com o banco de dados
 		Connection con = c.conectar();
 		
-		String query = "INSERT INTO";
+		String query = "INSERT INTO genero (id_jogo, 'nome_jogo') VALUES (1, Bloodborne)";
 		
 		try {
 			PreparedStatement ps = con.prepareStatement(query);
+			
+			//seta parametros
+			ps.setInt(1, p.getIdjogo());
+			ps.setString(2, p.getNomejogo());
+			
+			//Consolidar a conexão do comando no banco
+			ps.executeUpdate();
+			
+			//fecha a conexão
+			c.fecharConexao();
+			
+			return true;
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		return true;
+		return false;
 	}
 	
 }
