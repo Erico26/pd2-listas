53fd180e936000af4d997b63ecedd3dd87f136bb
Eric <eric.o26@aluno.ifsc.edu.br>
2023-08-15 10:08:20 -0300

    pedaço de listar msql parte2

--- src/main/java/controle/GeneroDAO.java
+++ src/main/java/controle/GeneroDAO.java
@@ -2,7 +2,9 @@ package controle;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import com.mysql.cj.xdevapi.PreparableStatement;
 
@@ -10,6 +12,32 @@ import modelo.Genero;
 
 public class GeneroDAO {
 
+	
+	public ArrayList<Genero> listar(){
+		 Conexao c = Conexao.getInstancia();
+		 
+		 Connection con = c.conectar();
+		 
+		 ArrayList<Genero> generos = new ArrayList();
+		 
+		 String query = "SELECT * from genero";
+		 try {
+				PreparedStatement ps = con.prepareStatement(query);
+				ResultSet rs = ps.executeQuery();
+				while(rs.next()) {
+					
+				}
+				
+		 }catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		 
+		 return generos;
+	}
+	
+	
+	
 	public boolean inserir(Genero p) {
 		//intanciar
 		Conexao c = Conexao.getInstancia();
@@ -17,7 +45,7 @@ public class GeneroDAO {
 		//abrir instancia com o banco de dados
 		Connection con = c.conectar();
 		
-		String query = "INSERT INTO genero (id_jogo, 'nome_jogo') VALUES (1, Bloodborne)";
+		String query = "INSERT INTO genero " + "(id_jogo, 'nome_jogo')" + "VALUES (?, ?)";
 		
 		try {
 			PreparedStatement ps = con.prepareStatement(query);
